152cd2c71347cdab5ea436ae677f12090f4d2023
MasterSun81 <74555798+MasterSun8@users.noreply.github.com>
2022-03-31 20:29:49 +0000

    database works

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 
+const log = 'public/log.json'
+const link = 'https://agarripoff-1.mastersun81.repl.co/score'
+
+const logger = fs.createWriteStream(log, {
+  flags: 'a'
+})
+const writeLine = (line) => logger.write(`\n${line}`)
+
 app.use(express.static('public'))
 
 app.get('/score', (req, res) => {
-  res.send('helol')
+  let temp = fs.readFileSync(log).toString()
+  res.send(temp)
 })
 
 app.listen(8080)
--- public/agar.js
+++ public/agar.js
@@ -4,121 +4,143 @@ canvas.height = window.innerHeight
 body = document.getElementsByTagName("body")[0]
 body.appendChild(canvas)
 const context = canvas.getContext('2d')
-const randomColor   = size  => [...Array(size)].map(() => randomInt(16).toString(16)).join('')
+const randomColor = size => [...Array(size)].map(() => randomInt(16).toString(16)).join('')
 const fps = 60
 const Canvasx = canvas.width
 const Canvasy = canvas.height
-var scale = 1
+const url = 'https://agarripoff-1.mastersun81.repl.co/score'
 
-var mousex = Canvasx/2
-var mousey = Canvasy/2
+var highestScore = 0
+var scale = 1
+var text = ''
+var mousex = Canvasx / 2
+var mousey = Canvasy / 2
+
+
 
 body.addEventListener("mousemove", function(event) {
-    myFunction(event)
+  myFunction(event)
 })
-  
+
 function myFunction(e) {
-    mousex = e.clientX
-    mousey = e.clientY
+  mousex = e.clientX
+  mousey = e.clientY
 }
 
-function randomInt(max){
-    let temp = Math.floor(Math.random() * max)
-    if(Math.round(Math.random())){
-        temp *= -1
-    }else{
-        temp *= 1
-    }
-    console.log(temp)
-    return  temp
+async function createLeaderboard(){
+  try {
+    let response = await fetch(url)
+    text = await response.json()
+    let temp = 0
+    text.forEach(obj => {
+      if(temp < obj.score) temp = obj.score
+    })
+    highestScore = temp
+  } catch (e) {
+    console.log(e)
+  }
+  
+}
+
+function randomInt(max) {
+  let temp = Math.floor(Math.random() * max)
+  if (Math.round(Math.random())) {
+    temp *= -1
+  } else {
+    temp *= 1
+  }
+  return temp
 }
 
 arrayOfFood = []
 
-class Blob{
-    constructor(){
-        this.color = '#' + randomColor(6)
-        this.mass = 16
-		this.r = this.mass*scale
-        this.x = randomInt(Canvasx*20)
-        this.y = randomInt(Canvasy*20)
-        this.velocityx = 0
-        this.velocityy = 0
-    }
+class Blob {
+  constructor() {
+    this.color = '#' + randomColor(6)
+    this.mass = 16
+    this.r = this.mass * scale
+    this.x = randomInt(Canvasx * 20)
+    this.y = randomInt(Canvasy * 20)
+    this.velocityx = 0
+    this.velocityy = 0
+  }
 
-    movement(){
-        let distancex = mousex - hero.x
-        let distancey = mousey - hero.y
-        this.x += distancex / -50
-        this.y += distancey / -50
-    }
-    
-	scaleSize(){
-		this.r = this.mass*scale
-	}
+  movement() {
+    let distancex = mousex - hero.x
+    let distancey = mousey - hero.y
+    this.x += distancex / -50
+    this.y += distancey / -50
+  }
 
-    x(v){this.x = v}
-    y(v){this.y = v}
+  scaleSize() {
+    this.r = this.mass * scale
+  }
 
-    drawBlob(){
-        context.fillStyle = this.color
-        context.beginPath()
-        context.arc(this.x, this.y, this.r, 0, 2 * Math.PI)
-        context.fill()
-    }
+  x(v) { this.x = v }
+  y(v) { this.y = v }
+
+  drawBlob() {
+    context.fillStyle = this.color
+    context.beginPath()
+    context.arc(this.x, this.y, this.r, 0, 2 * Math.PI)
+    context.fill()
+  }
 }
 
-class mainBlob extends Blob{
-    constructor(){
-        super()
-        this.mass *= 2
-        this.x = Canvasx/2
-        this.y = Canvasy/2
-    }
-	
-    collidesWithFood(){
-        arrayOfFood.forEach(food => {
-        let pita = (this.x - food.x) * (this.x - food.x) + (this.y - food.y) * (this.y - food.y)
-        pita = Math.sqrt(pita)
-        let radSum = this.r + food.r/2
-        if(pita <= radSum){
-            food.x = randomInt(Canvasx)
-            food.y = randomInt(Canvasy)
-            this.increaseSize()
-        }
-        })
-    }
+class mainBlob extends Blob {
+  constructor() {
+    super()
+    this.mass *= 2
+    this.x = Canvasx / 2
+    this.y = Canvasy / 2
+  }
 
-    score(){
-        context.font = "30px Arial"
-        context.fillStyle = "black"
-        context.fillText(('mass: ' + this.mass) ,10 ,50)
-    }
+  collidesWithFood() {
+    arrayOfFood.forEach(food => {
+      let pita = (this.x - food.x) * (this.x - food.x) + (this.y - food.y) * (this.y - food.y)
+      pita = Math.sqrt(pita)
+      let radSum = this.r + food.r / 2
+      if (pita <= radSum) {
+        food.x = randomInt(Canvasx)
+        food.y = randomInt(Canvasy)
+        this.increaseSize()
+      }
+    })
+  }
 
-    increaseSize(){
-        this.mass += 1
-        scale = (scale/100)*99
-        console.log(scale)
-    }
+  score() {
+    context.font = "30px Arial"
+    context.fillStyle = "black"
+    context.fillText(('Highest Score: ' + highestScore), Canvasx-300, 50)
+    context.fillText(('Your Mass: ' + this.mass), 10, 50)
+  }
+
+  increaseSize() {
+    this.mass += 1
+    scale = (scale / 100) * 99
+  }
 }
 
 const hero = new mainBlob()
 hero.scaleSize()
 
-for(let x = 0; x < 10000; x++){
-    arrayOfFood.push(new Blob())
+for (let x = 0; x < 10000; x++) {
+  arrayOfFood.push(new Blob())
 }
 
-function main(){
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    arrayOfFood.forEach(x => {
-        x.movement()
-        x.scaleSize()
-		x.drawBlob()
-    })
-    hero.drawBlob()
-    hero.collidesWithFood(arrayOfFood)
-    hero.score()
+function main() {
+  createLeaderboard()
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  arrayOfFood.forEach(x => {
+    x.movement()
+    x.scaleSize()
+    x.drawBlob()
+  })
+  hero.drawBlob()
+  hero.collidesWithFood(arrayOfFood)
+  hero.score()
 }
 
-setInterval(main, 1000/fps)
+createLeaderboard()
+
+setInterval(main, 1000 / fps)
