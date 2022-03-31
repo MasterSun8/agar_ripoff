3c9268d2d433ee6970819ce8cd1c7bf7eb12cea5
MasterSun81 <74555798+MasterSun8@users.noreply.github.com>
2022-03-31 18:49:15 +0000

    works under "https://agarripoff-1.mastersun81.repl.co"

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const path = require('path')
-app.use('/static', express.static('/public'))
+const fs = require('fs')
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/score', (req, res) => {
+  res.send('helol')
+})
+
+app.listen(8080)
